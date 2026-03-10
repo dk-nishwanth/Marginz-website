c48360c1b990af48a2ff5f277d2eb263abe800dc
MARGINZ Developer <dev@marginz.com>
2026-03-10 14:57:21 +0530

    home page issue

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App.tsx';
-import AboutPage from './AboutPage.tsx';
-import ServicesPage from './ServicesPage.tsx';
-import ContactPage from './ContactPage.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/about" element={<AboutPage />} />
-        <Route path="/services" element={<ServicesPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-      </Routes>
-    </Router>
+    <App />
   </StrictMode>,
 );
