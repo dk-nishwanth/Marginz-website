a3bd62ec12f50fb1168f8f74e769405badc2f39c
MARGINZ Developer <dev@marginz.com>
2026-03-10 15:00:42 +0530

    deployment issue

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ export default function App() {
         <Route path="/about" element={<AboutPage />} />
         <Route path="/services" element={<ServicesPage />} />
         <Route path="/contact" element={<ContactPage />} />
+        <Route path="*" element={<HomePage />} />
       </Routes>
     </Router>
   );
