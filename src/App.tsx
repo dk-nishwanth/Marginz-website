bf9c75f640ce5b9b6bad28adc3b674785f93e1b0
MARGINZ Developer <dev@marginz.com>
2026-03-11 11:38:24 +0530

    nav link issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import ServicesPage from './ServicesPage';
@@ -8,6 +9,7 @@ import ContactPage from './ContactPage';
 export default function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
